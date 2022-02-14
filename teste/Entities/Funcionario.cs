f89290e850646bc4c5ee7536872050145ad4236d
Damiao <damiao.jose@hotmail.com>
2022-02-14 14:44:49 -0300

    Codigo sofrido pronto

---
+++ teste/Entities/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroRelatorio.Entities
+    {
+    class Funcionario
+        {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario()
+            {
+            }
+        public Funcionario(int id, string nome, DateTime dataNascimento, string cargo, double salario)
+            {
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Cargo = cargo;
+            Salario = salario;
+            }
+        public void AumentarSalario(double porcentagem)
+            {
+            Salario += Salario * porcentagem / 100;
+            }
+        public override string ToString()
+            {
+            return $"{Id}, {Nome}, {DataNascimento.ToString("d")}, {Cargo}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+            }
+
+        }
+    }
---
+++ teste/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CadastroRelatorio.Entities;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CadastroRelatorio
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            try
+                {
+                Console.Write("Quantos funcionarios quer cadastrar? ");
+                int n = int.Parse(Console.ReadLine());
+                List<Funcionario> fun = new List<Funcionario>();
+
+                for (int i = 1; i <= n; i++)
+                    {
+                    Console.WriteLine($"Funcionario #{i}");
+                    Console.Write("Qual o ID? ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Data de nascimento: ");
+                    DateTime datanascimento = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Cargo: ");
+                    string cargo = Console.ReadLine();
+                    Console.Write("Salario: ");
+                    double salario = double.Parse(Console.ReadLine());
+
+                    fun.Add(new Funcionario(id, nome, datanascimento, cargo, salario));
+                    }
+
+                foreach (Funcionario f in fun)
+                    {
+                    Console.WriteLine(f);
+                    }
+                Console.WriteLine("Onde quer salvar essa lista de funcionarios?(Caminho c: ");
+                string caminho = Console.ReadLine();
+
+                using StreamWriter sw = File.AppendText(caminho);
+                foreach (Funcionario f in fun)
+                    {
+                    sw.WriteLine(f.ToString());
+                    }
+                }
+            catch (Exception e)
+                {
+                Console.WriteLine("Ocorreu um Erro: " + e.Message);
+                }
+            }
+        }
+    }
